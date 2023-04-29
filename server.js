f6c347c66aef17c96fcced13617a696b0032c617
Rafael Giuriatto <rafagiuriatto@hotmail.com>
2023-04-28 21:19:43 -0300

    Aula 2

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const http = require('http');
-const port = 8000
+import app from './src/app.js'
+const port = process.env.PORT || 8000
 
-const rotas = {
-    '/': 'Hello World',
-    '/livros': 'Bem vindo à biblioteca',
-    '/autores': 'Lista de autores'
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(rotas[req.url]);
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server escutando em http://localhost:${port}
     Ctrl C para encerrar`)
 })
